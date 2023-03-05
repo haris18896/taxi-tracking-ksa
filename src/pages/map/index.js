dd419944bba8eda0d0887ce6a35d0262011b5add
haris18896 <haris18896@gmail.com>
2023-03-05 03:44:57 -0800

    pickup and dropoff points has been drawn

--- src/pages/map/index.js
+++ src/pages/map/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import 'mapbox-gl/dist/mapbox-gl.css'
 
 // ** MUI Imports
@@ -9,11 +9,33 @@ import { useRouter } from 'next/router'
 const Home = () => {
   const router = useRouter()
   const { pickup, dropoff } = router.query
-  console.log('pickup : ', pickup, 'dropoff : ', dropoff)
+  const [center, setCenter] = useState({ latitude: null, longitude: null })
+
+  const pickUpCoordinates = pickup && [pickup.split(',')[0], pickup.split(',')[1]]
+  const dropoffCoordinates = dropoff && [dropoff.split(',')[0], pickup.split(',')[1]]
+
+  console.log('pickupCoordinates : ', pickUpCoordinates, 'dropoffCoordinates : ', dropoffCoordinates)
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          setCenter({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+          })
+        },
+        error => {
+          console.log(error)
+        }
+      )
+    } else {
+      console.log('Geolocation is not supported by this browser.')
+    }
+  }, [])
 
   return (
     <>
-      <Map />
+      <Map center={center} pickUpCoordinates={pickUpCoordinates} dropoffCoordinates={dropoffCoordinates} />
     </>
   )
 }
--- src/views/map/index.js
+++ src/views/map/index.js
@@ -1,39 +1,44 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import mapboxgl from '!mapbox-gl'
 
 mapboxgl.accessToken =
   'pk.eyJ1IjoiaGFyaXN0cmFja2luZyIsImEiOiJjbGVneWQ3anowanJvM3ZsZDdiNTB2aGk2In0.-YLuxE0bmfGbf8x3GH3n7A'
 
 const Map = props => {
+  const map = useRef(null)
+  const mapContainer = useRef(null)
+
+  const addToMap = (map, coordinates) => {
+    const marker = new mapboxgl.Marker().setLngLat(coordinates).addTo(map)
+  }
+
   useEffect(() => {
     const map = new mapboxgl.Map({
-      container: 'map',
+      container: mapContainer.current,
       style: 'mapbox://styles/mapbox/dark-v11',
-      center: [-99.29011, 39.39172],
-      zoom: 3
+      center: [74.3451755, 31.5113502],
+      zoom: 9
     })
 
-    // if (props.pickUpCoordinates) {
-    //   addToMap(map, props.pickUpCoordinates)
-    // }
+    // addToMap(map, [74.3451755, 31.5113502])
 
-    // if (props.dropoffCoordinates) {
-    //   addToMap(map, props.dropoffCoordinates)
-    // }
+    if (props.pickUpCoordinates) {
+      addToMap(map, props.pickUpCoordinates)
+    }
 
-    // if (props.pickUpCoordinates && props.dropoffCoordinates) {
-    //   map.fitBounds([props.pickUpCoordinates, props.dropoffCoordinates], {
-    //     padding: 60
-    //   })
-    // }
+    if (props.dropoffCoordinates) {
+      addToMap(map, props.dropoffCoordinates)
+    }
+
+    if (props.pickUpCoordinates && props.dropoffCoordinates) {
+      map.fitBounds([props.pickUpCoordinates, props.dropoffCoordinates], {
+        padding: 60
+      })
+    }
   }, [props.pickUpCoordinates, props.dropoffCoordinates])
 
-  // const addToMap = (map, coordinates) => {
-  //   const marker = new mapboxgl.Marker().setLngLat(coordinates).addTo(map)
-  // }
-
-  return <div id='map'></div>
+  return <div ref={mapContainer} className='map-container' />
 }
 
 export default Map
